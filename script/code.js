95ef97f5b0750a485a51ea3c2ef2b43fe97579ab
mujahidfisher <fishermujahid@gmail.com>
2023-05-17 13:57:36 +0200

    finished exercise

---
+++ script/code.js
@@ -0,0 +1,31 @@
+// Q1
+// let firstName = "Mujahid"
+// console.log(typeof firstName);
+// typeof string 
+
+// Q2
+// let firstName = 50
+// console.log(typeof firstName);
+// typeof number
+
+// Q3
+// string 
+// let myName = "Fisher"
+// console.log(typeof myName);
+
+// number
+// let numb1 = 100
+// console.log(typeof numb1);
+
+// boolean
+// let firstColor = true 
+// console.log(typeof firstColor);
+
+// undefined 
+// let surname;
+// console.log(typeof surname);
+
+null
+let choiceColor = null
+console.log(typeof choiceColor);
+object
